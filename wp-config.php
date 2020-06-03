e3d8c127fb86963265705c65e3643819479c6962
kumarsank <54392377+kumarsank@users.noreply.github.com>
2020-06-03 18:28:37 +0530

    rere

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress');
+define( 'DB_NAME', 'word');
 
 /** MySQL database username */
-define( 'DB_USER', 'root');
+define( 'DB_USER', 'ro');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root');
+define( 'DB_PASSWORD', 'ro');
 
 /** MySQL hostname */
-define( 'DB_HOST', '127.0.0.1');
+define( 'DB_HOST', '127.0');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
